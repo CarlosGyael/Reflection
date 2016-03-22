2a65f17ea6e565407b6887af0859832d9455e899
Carlos Gutierrez <carlos_gutierrezz99@hotmail.com>
2016-03-22 18:07:48 -0500

    added day 17

---
+++ Day17.d
@@ -0,0 +1,4 @@
+Today i finished my grocery store project and will make it more readable tommorrow.
+i also learned how to edit text files within c# , pretty interesting i must say.
+what i put in my project was people applying to the store thus making you more money.
+i also added a txt file keeping track of the high scores.
